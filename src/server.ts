83913a48faacc5d47b74e8a7f78d7f3bf909c853
Elian Becali <elianbecaliaguiar@gmail.com>
2020-10-14 06:08:56 -0400

    criando um orfanato

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,46 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello world'});
+app.use(express.json());
+
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+  
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.json({ 
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends, });
 });
 
 app.listen(3333);
